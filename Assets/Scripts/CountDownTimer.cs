6703230e3cdcb7cc09fc327ed22bbed85efdf586
JianmingChen <chenjianming697@gmail.com>
2021-10-13 19:54:50 -0700

    updated coding style

--- Assets/Scripts/CountDownTimer.cs
+++ Assets/Scripts/CountDownTimer.cs
@@ -1,38 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CountDownTimer : MonoBehaviour
 {
-    public float timeValue;
-    public Text texTimer;
+    public float TimeValue;
+    public Text TexTimer;
     
-    void Update()
-    {
-        if (timeValue > 0)
-        {
-            timeValue -= Time.deltaTime;
-        }
-        else
-        {
-            timeValue = 0;
+    void Update() {
+
+        if (TimeValue > 0){
+            TimeValue -= Time.deltaTime;
+        } else {
+            TimeValue = 0;
             SceneManager.LoadScene("GameOverScene");
         }
-        DisplayCountDownTime(timeValue);
+
+        DisplayCountDownTime(TimeValue);
 
     }
 
-    void DisplayCountDownTime(float DisplayTheTime)
-    {
+    void DisplayCountDownTime(float DisplayTheTime) {
 
-        if (DisplayTheTime < 0)
-        {
+        if (DisplayTheTime < 0) {
             DisplayTheTime = 0;
-        }
-        else if (DisplayTheTime > 0)
-        {
+        } else if (DisplayTheTime > 0) {
             DisplayTheTime += 1;
         }
 
@@ -40,6 +32,6 @@ public class CountDownTimer : MonoBehaviour
         float seconds = Mathf.FloorToInt(DisplayTheTime % 60);
         float millsec = DisplayTheTime % 1 * 1000;
 
-        texTimer.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, millsec);
+        TexTimer.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, millsec);
     }
 }
