b23e32fbf125ce19955c421202131e145ffd4f06
Charles <charleshuang97@gmail.com>
2021-11-09 04:43:22 -0800

    MovingBlobs can now move up/down, forward/backward, OR left/right

--- Assets/Scripts/Blobs/MovingBlob.cs
+++ Assets/Scripts/Blobs/MovingBlob.cs
@@ -1,40 +1,63 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 // They move n' groove
 public class MovingBlob : Blob {
     [SerializeField] private float _directionChangeSpeed;
+    [SerializeField] private WorldDirection _worldDirection;
+/*    [SerializeField] private LinearDirection _x;
+    [SerializeField] private LinearDirection _y;
+    [SerializeField] private LinearDirection _z;*/
+
     private Vector3 _direction;
     private CharacterController _controller;
     private Coroutine _currentCoroutine;
     void Start() {
         _controller = GetComponent<CharacterController>();
-        if (Random.Range(0, 2) == 0) {
-            _direction = Vector3.right;
-        } else {
-            _direction = Vector3.left;
-        }
+        ChangeWorldDirection(_worldDirection);
 
-        _currentCoroutine = StartCoroutine(ChangeDirectionCoroutine());
+        if (Random.Range(0, 2) == 0) {
+            _direction *= -1;
+        } 
+
+        _currentCoroutine = StartCoroutine(ReverseDirectionCoroutine());
     }
 
     void FixedUpdate() {
         _controller.Move(_direction * Speed * Time.fixedDeltaTime);
     }
 
-    private void ChangeDirection() {
+    private void ReverseDirection() {
         StopCoroutine(_currentCoroutine);
-        _currentCoroutine = StartCoroutine(ChangeDirectionCoroutine());
+        _currentCoroutine = StartCoroutine(ReverseDirectionCoroutine());
         _direction *= -1;
     }
 
-    IEnumerator ChangeDirectionCoroutine() {
+    public void ChangeWorldDirection(WorldDirection dir) {
+        switch (dir) {
+            case WorldDirection.X:
+                _direction = Vector3.right;
+                break;
+            case WorldDirection.Y:
+                _direction = Vector3.up;
+                break;
+            case WorldDirection.Z:
+                _direction = Vector3.forward;
+                break;
+            default:
+                throw new ArgumentException("Unknown 'Direction' enum value: " + dir);
+        }
+    }
+
+    IEnumerator ReverseDirectionCoroutine() {
         yield return new WaitForSeconds(_directionChangeSpeed);
-        ChangeDirection();
+        ReverseDirection();
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit) {
-        ChangeDirection();
+        ReverseDirection();
     }
 
     private void OnValidate() {
@@ -42,4 +65,17 @@ public class MovingBlob : Blob {
             _directionChangeSpeed = 0;
         }
     }
+}
+
+public enum WorldDirection {
+    X,
+    Y,
+    Z
+}
+
+public enum LinearDirection {
+    Off,
+    Pos,
+    Neg,
+    Rand
 }
