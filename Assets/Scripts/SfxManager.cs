e82edf6f8cddbdf47fedce12e8e3ee398109b17c
JianmingChen <chenjianming697@gmail.com>
2021-11-11 23:35:17 -0800

    added bgm and button sfx

---
+++ Assets/Scripts/SfxManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+    public AudioSource AudioSfx;
+
+    public AudioClip AudioClick;
+
+    public static SfxManager sfxInstance;
+
+    private void Awake()
+    {
+        if (sfxInstance != null && sfxInstance != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        sfxInstance = this;
+        DontDestroyOnLoad(this);
+    }
+}
