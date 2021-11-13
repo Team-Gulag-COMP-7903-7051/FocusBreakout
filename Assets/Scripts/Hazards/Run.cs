d2ee3e5e3f8d6ebcd75038fa7b908a945fc2ac76
Charles <charleshuang97@gmail.com>
2021-11-13 03:39:38 -0800

    expanded blob spawn point

--- Assets/Scripts/Hazards/Run.cs
+++ Assets/Scripts/Hazards/Run.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using UnityEngine;
 
+// Made for fun
 public class Run : MonoBehaviour { 
     [SerializeField] private GameObject _blobManager;
     [SerializeField] private GameObject _shooter;
-    [SerializeField ]private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSource;
 
-    [SerializeField] private float _trackStartTime;
-    [SerializeField] private float _time;
-    void Start()
-    {
+    private const float _trackStartTime = 128;
+    private const float _time = 4.5f;
+
+    void Start() {
         _blobManager.SetActive(false);
         _shooter.SetActive(false);
         _audioSource.time = _trackStartTime;
