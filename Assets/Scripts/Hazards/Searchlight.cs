361caa828227fa745c48ef6c67de1cd69ff219d8
acm <alexis_cm@live.com>
2021-11-14 22:59:58 -0800

    Added basic Searchlight movemement - needs improvement

--- Assets/Scripts/Hazards/Searchlight.cs
+++ Assets/Scripts/Hazards/Searchlight.cs
@@ -6,8 +6,33 @@ using UnityEngine;
 public class Searchlight : MonoBehaviour {
     [SerializeField] private Transform _thePlayer;
 
+    [SerializeField] private float _minX;
+    [SerializeField] private float _maxX;
+    [SerializeField] private float _minZ;
+    [SerializeField] private float _maxZ;
+
+    [SerializeField] private float _speed;  // Searchlight's movement speed
+
+    private Vector3 _targetPos;
+    private bool _isMoving = true;
+
+    private void Start() {
+        _targetPos = transform.position;
+    }
+
     void FixedUpdate() {
         Search();
+    }
+
+    private Vector2 GetRandomTarget() {
+        float x = Random.Range(_minX, _maxX);
+        float y = transform.position.y;
+        float z = Random.Range(_minZ, _maxZ);
+        return new Vector3(x, y, z);
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        _isMoving = false;
     }
 
     /// <summary>
@@ -18,13 +43,24 @@ public class Searchlight : MonoBehaviour {
         Vector3 origin = transform.position;
         Vector3 dir = transform.forward;
         float radius = 5f;
-        float castLen = 20;
+        float castLen = 20f;
         RaycastHit hit;
 
         if (Physics.SphereCast(origin, radius, dir, out hit, castLen)) {
 
             if (hit.collider.CompareTag("Blob")) {
                 print("hello there");
+                // Trigger FollowPlayer()
+                // Trigger "Bombardment" method to attack player at this pos
+            }
+        }
+
+        if (_isMoving) {
+            if (transform.position == _targetPos) {
+                _targetPos = GetRandomTarget();
+            } else {
+                float step = _speed * Time.fixedDeltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, _targetPos, step);
             }
         }
     }
