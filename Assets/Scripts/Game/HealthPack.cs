6defcbf945b65f8ca3cb4558641dd435ab63173f
Charles <charleshuang97@gmail.com>
2021-11-16 03:21:54 -0800

    added item class

--- Assets/Scripts/Game/HealthPack.cs
+++ Assets/Scripts/Game/HealthPack.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
 
-public class HealthPack : MonoBehaviour {
+public class HealthPack : Item {
     [SerializeField] private int _healFactor;
 
-    private void OnTriggerEnter(Collider col) {
-        Player player = col.gameObject.GetComponent<Player>();
-        if (player != null && player.CurrentHealth != player.MaxHealth) {
+    protected override void OnTriggerAction(Player player) {
+        if (player.CurrentHealth != player.MaxHealth) {
             player.Heal(_healFactor);
             Destroy(gameObject);
-        }  
+        }
     }
 
     private void OnValidate() {
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+// Requires a collider to work
+// Should be extended by classes if the player can pick it up
+public abstract class Item : MonoBehaviour
+{
+    // Ensures the item being picked up is by the Player
+    private void OnTriggerEnter(Collider col) {
+        Player player = col.gameObject.GetComponent<Player>();
+
+        if (player != null) {
+            OnTriggerAction(player);
+        }
+    }
+
+    // What to do when OnTriggerEnter is triggered
+    protected abstract void OnTriggerAction(Player player);
+}
