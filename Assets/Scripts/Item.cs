9eb15ca0c017a1a050277a5f441564672d2d5169
Charles <charleshuang97@gmail.com>
2021-11-16 17:49:02 -0800

    code cleanup

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 // Requires a collider to work
 // Should be extended by classes if the player can pick it up
-public abstract class Item : MonoBehaviour
-{
+public abstract class Item : MonoBehaviour {
     // Ensures the item being picked up is by the Player
     private void OnTriggerEnter(Collider col) {
         Player player = col.gameObject.GetComponent<Player>();
--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
-public class Key : Item
-{
+public class Key : Item {
     protected override void OnTriggerAction(Player player) {
         Debug.Log("Object hit by Player");
         gameObject.SetActive(false);
