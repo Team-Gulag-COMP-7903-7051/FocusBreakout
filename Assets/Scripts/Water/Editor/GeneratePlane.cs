8d66f7e362861e940299c350c621ef5293013b1d
Mohammed Bajaman <mbajaman@my.bcit.ca>
2021-11-17 21:06:03 -0800

    Fixed code styling

--- Assets/Scripts/Water/Editor/GeneratePlane.cs
+++ Assets/Scripts/Water/Editor/GeneratePlane.cs
@@ -2,95 +2,82 @@
 using UnityEditor;
 using System.IO;
 
-namespace LowPolyWater
-{
-    public class GeneratePlane : ScriptableWizard
-    {
-        public string objectName;           //Optional name that can given to created plane gameobject
+namespace LowPolyWater {
+    public class GeneratePlane : ScriptableWizard {
+        public string ObjectName;           //Optional name that can given to created plane gameobject
 
-        public int widthSegments = 1;       //Number of pieces for dividing plane vertically
-        public int heightSegments = 1;      //Number of pieces for dividing plane horizontally
-        public float planeWidth = 1.0f;     
-        public float planeHeight = 1.0f;    
+        public int WidthSegments = 1;       //Number of pieces for dividing plane vertically
+        public int HeightSegments = 1;      //Number of pieces for dividing plane horizontally
+        public float PlaneWidth = 1.0f;     
+        public float PlaneHeight = 1.0f;    
 
-        public bool addCollider = false;    //Add box collider?
-        public Material material;           //By default, it is assigned to 'LowPolyWaterMaterial' in the editor
+        public bool AddCollider = false;    //Add box collider?
+        public Material Material;           //By default, it is assigned to 'LowPolyWaterMaterial' in the editor
 
         static Camera cam;
         static Camera lastUsedCam;
 
         //Generated plane meshes are saved and loaded from Plane Meshes folder (you can change it to whatever you want)
-        public static string assetSaveLocation = "Assets/Low Poly Water/Plane Meshes/";
+        public static string AssetSaveLocation = "Assets/Low Poly Water/Plane Meshes/";
 
         [MenuItem("GameObject/LowPoly Water/Generate Water Plane...")]
-        static void CreateWizard()
-        {
+        static void CreateWizard() {
             cam = Camera.current;
             // Hack because camera.current doesn't return editor camera if scene view doesn't have focus
-            if (!cam)
-            {
+            if (!cam) {
                 cam = lastUsedCam;
-            }
-            else
-            {
+            } else {
                 lastUsedCam = cam;
             }
 
             //Check if the asset save location folder exists
             //If the folder doesn't exists, create it
-            if (!Directory.Exists(assetSaveLocation))
-            {
-                Directory.CreateDirectory(assetSaveLocation);
+            if (!Directory.Exists(AssetSaveLocation)) {
+                Directory.CreateDirectory(AssetSaveLocation);
             }
 
             //Open Wizard
             DisplayWizard("Generate Water Plane", typeof(GeneratePlane));
         }
 
-        void OnWizardUpdate()
-        {
+        void OnWizardUpdate() {
             //Max segment number is 254, because a mesh can't have more 
             //than 65000 vertices (254^2 = 64516 max. number of vertices)
-            widthSegments = Mathf.Clamp(widthSegments, 1, 254);
-            heightSegments = Mathf.Clamp(heightSegments, 1, 254);
+            WidthSegments = Mathf.Clamp(WidthSegments, 1, 254);
+            HeightSegments = Mathf.Clamp(HeightSegments, 1, 254);
         }
 
-        private void OnWizardCreate()
-        {
+        private void OnWizardCreate() {
             //Create an empty gamobject
             GameObject plane = new GameObject();
 
             //If user hasn't assigned a name, by default object name is 'Plane'
-            if (string.IsNullOrEmpty(objectName))
-            {
+            if (string.IsNullOrEmpty(ObjectName)) {
                 plane.name = "Plane";
-            }
-            else
-            {
-                plane.name = objectName;
+            } else {
+                plane.name = ObjectName;
             }
 
             //Create Mesh Filter and Mesh Renderer components
             MeshFilter meshFilter = plane.AddComponent(typeof(MeshFilter)) as MeshFilter;
             MeshRenderer meshRenderer = plane.AddComponent((typeof(MeshRenderer))) as MeshRenderer;
-            meshRenderer.sharedMaterial = material;
+            meshRenderer.sharedMaterial = Material;
 
             //Generate a name for the mesh that will be created
-            string planeMeshAssetName = plane.name + widthSegments + "x" + heightSegments
-                                        + "W" + planeWidth + "H" + planeHeight + ".asset";
+            string planeMeshAssetName = plane.name + WidthSegments + "x" + HeightSegments
+                                        + "W" + PlaneWidth + "H" + PlaneHeight + ".asset";
 
             //Load the mesh from the save location
-            Mesh m = (Mesh)AssetDatabase.LoadAssetAtPath(assetSaveLocation + planeMeshAssetName, typeof(Mesh));
+            Mesh m = (Mesh)AssetDatabase.LoadAssetAtPath(AssetSaveLocation + planeMeshAssetName, typeof(Mesh));
 
             //If there isn't a mesh located under assets, create the mesh
-            if (m == null)
-            {
+            if (m == null) {
                 m = new Mesh();
                 m.name = plane.name;
 
-                int hCount2 = widthSegments + 1;
-                int vCount2 = heightSegments + 1;
-                int numTriangles = widthSegments * heightSegments * 6;
+                int hCount2 = WidthSegments + 1;
+                int vCount2 = HeightSegments + 1;
+                int numTriangles = WidthSegments * HeightSegments * 6;
                 int numVertices = hCount2 * vCount2;
 
                 Vector3[] vertices = new Vector3[numVertices];
@@ -101,17 +88,15 @@ namespace LowPolyWater
                 Vector2 anchorOffset = Vector2.zero;
 
                 int index = 0;
-                float uvFactorX = 1.0f / widthSegments;
-                float uvFactorY = 1.0f / heightSegments;
-                float scaleX = planeWidth / widthSegments;
-                float scaleY = planeHeight / heightSegments;
+                float uvFactorX = 1.0f / WidthSegments;
+                float uvFactorY = 1.0f / HeightSegments;
+                float scaleX = PlaneWidth / WidthSegments;
+                float scaleY = PlaneHeight / HeightSegments;
 
                 //Generate the vertices
-                for (float y = 0.0f; y < vCount2; y++)
-                {
-                    for (float x = 0.0f; x < hCount2; x++)
-                    {
-                        vertices[index] = new Vector3(x * scaleX - planeWidth / 2f - anchorOffset.x, 0.0f, y * scaleY - planeHeight / 2f - anchorOffset.y);
+                for (float y = 0.0f; y < vCount2; y++) {
+                    for (float x = 0.0f; x < hCount2; x++) {
+                        vertices[index] = new Vector3(x * scaleX - PlaneWidth / 2f - anchorOffset.x, 0.0f, y * scaleY - PlaneHeight / 2f - anchorOffset.y);
 
                         tangents[index] = tangent;
                         uvs[index++] = new Vector2(x * uvFactorX, y * uvFactorY);
@@ -120,10 +105,8 @@ namespace LowPolyWater
 
                 //Reset the index and generate triangles
                 index = 0;
-                for (int y = 0; y < heightSegments; y++)
-                {
-                    for (int x = 0; x < widthSegments; x++)
-                    {
+                for (int y = 0; y < HeightSegments; y++) {
+                    for (int x = 0; x < WidthSegments; x++) {
                         triangles[index] = (y * hCount2) + x;
                         triangles[index + 1] = ((y + 1) * hCount2) + x;
                         triangles[index + 2] = (y * hCount2) + x + 1;
@@ -143,7 +126,7 @@ namespace LowPolyWater
                 m.RecalculateNormals();
 
                 //Save the newly created mesh under save location to reload later
-                AssetDatabase.CreateAsset(m, assetSaveLocation + planeMeshAssetName);
+                AssetDatabase.CreateAsset(m, AssetSaveLocation + planeMeshAssetName);
                 AssetDatabase.SaveAssets();
             }
 
@@ -152,8 +135,9 @@ namespace LowPolyWater
             m.RecalculateBounds();
 
             //If add collider is set to true, add a box collider
-            if (addCollider)
+            if (AddCollider) {
                 plane.AddComponent(typeof(BoxCollider));
+            }
 
             //Add LowPolyWater as component
             plane.AddComponent<LowPolyWater>();
--- Assets/Scripts/Water/LowPolyWater.cs
+++ Assets/Scripts/Water/LowPolyWater.cs
@@ -1,12 +1,10 @@
 ﻿using UnityEngine;
 
-namespace LowPolyWater
-{
-    public class LowPolyWater : MonoBehaviour
-    {
-        public float waveHeight = 0.5f;
-        public float waveFrequency = 0.5f;
-        public float waveLength = 0.75f;
+namespace LowPolyWater {
+    public class LowPolyWater : MonoBehaviour {
+        public float WaveHeight = 0.5f;
+        public float WaveFrequency = 0.5f;
+        public float WaveLength = 0.75f;
 
         //Position where the waves originate from
         public Vector3 waveOriginPosition = new Vector3(0.0f, 0.0f, 0.0f);
@@ -15,14 +13,12 @@ namespace LowPolyWater
         Mesh mesh;
         Vector3[] vertices;
 
-        private void Awake()
-        {
+        private void Awake() {
             //Get the Mesh Filter of the gameobject
             meshFilter = GetComponent<MeshFilter>();
         }
 
-        void Start()
-        {
+        void Start() {
             CreateMeshLowPoly(meshFilter);
         }
 
@@ -31,8 +27,7 @@ namespace LowPolyWater
         /// </summary>
         /// <param name="mf">Mesh filter of gamobject</param>
         /// <returns></returns>
-        MeshFilter CreateMeshLowPoly(MeshFilter mf)
-        {
+        MeshFilter CreateMeshLowPoly(MeshFilter mf) {
             mesh = mf.sharedMesh;
 
             //Get the original vertices of the gameobject's mesh
@@ -45,8 +40,7 @@ namespace LowPolyWater
             Vector3[] vertices = new Vector3[triangles.Length];
             
             //Assign vertices to create triangles out of the mesh
-            for (int i = 0; i < triangles.Length; i++)
-            {
+            for (int i = 0; i < triangles.Length; i++) {
                 vertices[i] = originalVertices[triangles[i]];
                 triangles[i] = i;
             }
@@ -61,8 +55,7 @@ namespace LowPolyWater
             return mf;
         }
         
-        void Update()
-        {
+        void Update() {
             GenerateWaves();
         }
 
@@ -70,10 +63,8 @@ namespace LowPolyWater
         /// Based on the specified wave height and frequency, generate 
         /// wave motion originating from waveOriginPosition
         /// </summary>
-        void GenerateWaves()
-        {
-            for (int i = 0; i < vertices.Length; i++)
-            {
+        void GenerateWaves() {
+            for (int i = 0; i < vertices.Length; i++){
                 Vector3 v = vertices[i];
 
                 //Initially set the wave height to 0
@@ -81,10 +72,10 @@ namespace LowPolyWater
 
                 //Get the distance between wave origin position and the current vertex
                 float distance = Vector3.Distance(v, waveOriginPosition);
-                distance = (distance % waveLength) / waveLength;
+                distance = (distance % WaveLength) / WaveLength;
 
                 //Oscilate the wave height via sine to create a wave effect
-                v.y = waveHeight * Mathf.Sin(Time.time * Mathf.PI * 2.0f * waveFrequency
+                v.y = WaveHeight * Mathf.Sin(Time.time * Mathf.PI * 2.0f * WaveFrequency
                 + (Mathf.PI * 2.0f * distance));
                 
                 //Update the vertex
