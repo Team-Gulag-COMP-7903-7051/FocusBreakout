bbaac8caa7abc19742c42fcc08a0df81b40bc0cb
Mohammed Bajaman <mbajaman@my.bcit.ca>
2021-11-17 22:30:15 -0800

    Added box collider on Level 3

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -35,16 +35,16 @@ public static class SaveManager {
         if (_levelDataArray != null) {
             switch (_levelDataArray.Length) {
                 case 1:
-                    level = 1;
+                    level = 0;
                     break;
                 case 2:
-                    level = 2;
+                    level = 1;
                     break;
                 case 3:
-                    level = 3;
+                    level = 2;
                     break;
                 case 4:
-                    level = 4;
+                    level = 3;
                     break;
                 default:
                     throw new ArgumentException(_levelDataArray.Length +
