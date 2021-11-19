0f56087317d4e66e0fbf76be3d8b84f9edf678c4
Charles <charleshuang97@gmail.com>
2021-11-18 20:14:32 -0800

    some code cleanup

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -3,31 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
     public GameObject PauseMenuByEsc;
-
     public static bool IsPaused;
 
-    // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-                SfxManager.sfxInstance.AudioSfx.PlayOneShot(SfxManager.sfxInstance.AudioClick);
             if (IsPaused) {
                 Resume();
-                SfxManager.sfxInstance.AudioSfx.PlayOneShot(SfxManager.sfxInstance.AudioClick);
             } else {
                 PauseGame();
-                SfxManager.sfxInstance.AudioSfx.PlayOneShot(SfxManager.sfxInstance.AudioClick);
             }
         }
     }
 
     public void Resume() {
         PauseMenuByEsc.SetActive(false);
-        Time.timeScale = 1f; //active the game
+        Time.timeScale = 1f; //activate the game
         IsPaused = false;
     }
 
     public void PauseGame() {
-        PauseMenuByEsc.SetActive(true);
+        gameObject.SetActive(true);
         Time.timeScale = 0f;
         IsPaused = true;
     }
@@ -35,6 +30,5 @@ public class MenuManager : MonoBehaviour {
     public void Home() {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenuScene");
-        SfxManager.sfxInstance.AudioSfx.PlayOneShot(SfxManager.sfxInstance.AudioClick);
     }
 }
