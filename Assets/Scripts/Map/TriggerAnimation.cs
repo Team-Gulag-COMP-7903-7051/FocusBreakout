3cc1b2f5d7d61ba70965e026344ba0eb85f5a57a
acm <alexis_cm@live.com>
2021-12-02 01:41:02 -0800

    Completed Area 5

--- Assets/Scripts/Map/TriggerAnimation.cs
+++ Assets/Scripts/Map/TriggerAnimation.cs
@@ -9,7 +9,7 @@ public class TriggerAnimation : MonoBehaviour {
     [SerializeField] protected GameObject _objToAnimate;  // The GameObject to be animated on trigger
     [SerializeField] protected string _paramName;         // Name of Animation's Trigger Parameter
 
-    private Animator _animator;
+    protected Animator _animator;
 
     private void OnTriggerEnter(Collider other) {
         _animator = _objToAnimate.GetComponent<Animator>();
